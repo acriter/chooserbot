5cc134ff92757d29b0166d5663ab1e232d35cf32
Colin Cowles <colin@intrepid.io>
2017-09-19 16:08:19 +0900

    moved files

--- website/server.py
+++ website/server.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request
-import chatbot.py
+import chatbot as cb
 app = Flask(__name__)
+
+token = 'sohelk7b417cwnzxgt4eyux34r1wd9'
 
 @app.route('/', methods=['GET','POST'])
 def test():
@@ -10,6 +12,8 @@ def test():
 @app.route('/join', methods=['GET','POST'])
 def result():
     if request.method == 'POST':
+        bot = cb.TwitchBot('acriterdev', 'vr0zkeoavnuf7kxmunaezpjpz8zcag', token, request.form['username'])
+        bot.start()
         #print("a", request.data)
         #print("b", request.form)
         #print("c", request.get_json())
